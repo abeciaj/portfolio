7ef4a7882708f42959ece00072d43d676a7be56b
abeciaj <abeciaj23@gmail.com>
2021-11-29 14:38:34 +0800

    Added animation in intro.

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,32 @@
+import "./intro.css"
+
 const Intro = () => {
     return (
         <div className="i">
-            Intro page
+            <div className="i-left">
+                <div className="i-left-wrapper">
+                    <h2 className="i-intro">Hello, My name is </h2>
+                    <h2 className="i-name">Jayllan Abecia</h2>
+                    <div className="i-title">
+                        <div className="i-title-wrapper">
+                            <div className="i-title-item">Devops SysAdmin</div>
+                            <div className="i-title-item">IT Support</div>
+                            <div className="i-title-item">IT Support</div>
+                            <div className="i-title-item">Freelancer</div>
+                        </div>
+                    </div>
+                    <div className="i-desc">
+                    Lorem ipsum dolor sit amet, consectetur 
+                    adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna 
+                    aliqua.
+                    </div>
+                </div>
+            </div>
+
+            <div className="i-right">
+                <img src="" alt="" className="i-img" />
+            </div>
         </div>
     )
 }
