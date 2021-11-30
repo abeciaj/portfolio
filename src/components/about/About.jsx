dfca87bf3407541fa795c86b45a0bdabcb7f09ae
abeciaj <abeciaj23@gmail.com>
2021-11-30 16:20:51 +0800

    Added about me.

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -11,7 +11,17 @@ const About = () => {
                 </div>
             </div>
             <div className="a-right">
-            
+            <h1 className="a-title">About Me</h1>
+            <p className="a-sub">
+                It is a long established fact that a reader will be distracted by the
+                readable content.
+            </p>
+            <p className="a-desc">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat duis aute irure dolor in reprehende.
+            </p>
             </div>
         </div>
     )
