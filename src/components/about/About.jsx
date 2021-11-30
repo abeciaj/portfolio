a095e227e6440ac308dd53906c01ae8171d4cbaf
abeciaj <abeciaj23@gmail.com>
2021-11-30 16:13:29 +0800

    Added image card.

---
+++ src/components/about/About.jsx
@@ -0,0 +1,20 @@
+import "./about.css"
+
+const About = () => {
+    return (
+        <div className="a">
+            <div className="a-left">
+                <div className="a-card bg"></div>
+                <div className="a-card">
+                    <img src="https://images.pexels.com/photos/3636994/pexels-photo-3636994.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" 
+                    alt="" />
+                </div>
+            </div>
+            <div className="a-right">
+            
+            </div>
+        </div>
+    )
+}
+
+export default About
