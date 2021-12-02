5533cc7e85ae46a9d185663a620401c5895b8ce3
abeciaj <abeciaj23@gmail.com>
2021-12-02 17:10:22 +0800

    Created context api from darkmode.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import { useContext, useState } from "react";
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
 import Intro from "./components/intro/Intro";
 import ProjectList from "./components/projectList/ProjectList";
 import Toggle from "./components/toggle/Toggle";
+import { ThemeContext } from "./context";
+
 
 const App = () => {
+
+  const theme = useContext(ThemeContext)
+  const darkMode = theme.state.darkMode;
   return (
-    <div>
-      <Toggle/>
+    <div style={{ backgroundColor:darkMode ? "#222" : "white", 
+      color:darkMode && "white",
+    }}>
+      <Toggle />
       <Intro/>
       <About/>
       <ProjectList/>
