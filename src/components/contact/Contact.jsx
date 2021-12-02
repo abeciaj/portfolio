b9dc8da2792b42c10256c5c1839dbc3fbd00c8e7
abeciaj <abeciaj23@gmail.com>
2021-12-02 15:19:51 +0800

    Added contact info.

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,53 @@
+import "./contact.css"
+import Phone from "../../img/phone.png"
+import Email from "../../img/email.png"
+import Address from "../../img/address.png"
+
 const Contact = () => {
     return (
         <div className="c">
-            contact
+            <div className="c-bg"></div>
+            <div className="c-wrapper">
+                <div className="c-left">
+                    <div className="c-title">
+                        <h1>
+                            Lest's Discuss your project
+                        </h1>   
+                    </div>
+                    <div className="c-info">
+                        <div className="c-info-item">
+                            <img className="c-icon" 
+                                src={Phone} 
+                                alt="" 
+                            />
+                            +1 234
+                        </div>
+                        <div className="c-info-item">
+                            <img className="c-icon" 
+                                src={Email} 
+                                alt="" 
+                            />
+                            test@test.com
+                        </div>
+                        <div className="c-info-item">
+                            <img className="c-icon" 
+                                src={Address} 
+                                alt="" 
+                            />
+                            <p>philippines</p>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div className="c-right">
+                    <p className="c-desc">
+                        <b>Lorem ipsum dolor sit amet</b>, consectetur 
+                        adipiscing elit, sed do eiusmod tempor 
+                        incididunt ut labore et dolore magna 
+                        aliqua.
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
