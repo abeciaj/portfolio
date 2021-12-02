f3be3c2d6a40067094ec35fd0a8c21841f7cda49
abeciaj <abeciaj23@gmail.com>
2021-12-02 11:25:07 +0800

    Update project image redirect link.

--- src/data.js
+++ src/data.js
@@ -2,32 +2,32 @@ export const projects = [
     {
       id: 1,
       img: "https://images.pexels.com/photos/9584136/pexels-photo-9584136.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-      link: "http://lama.dev",
+      link: "https://www.pexels.com/",
     },
     {
       id: 2,
       img: "https://images.pexels.com/photos/8652093/pexels-photo-8652093.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-      link: "http://lama.dev",
+      link: "https://www.pexels.com/",
     },
     {
       id: 3,
       img: "https://images.pexels.com/photos/9700071/pexels-photo-9700071.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-      link: "http://lama.dev",
+      link: "https://www.pexels.com/",
     },
     {
       id: 4,
       img: "https://images.pexels.com/photos/10219385/pexels-photo-10219385.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-      link: "http://lama.dev",
+      link: "https://www.pexels.com/",
     },
     {
       id: 5,
       img: "https://images.pexels.com/photos/10386945/pexels-photo-10386945.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-      link: "http://lama.dev",
+      link: "https://www.pexels.com/",
     },
     {
       id: 6,
       img: "https://images.pexels.com/photos/10211293/pexels-photo-10211293.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-      link: "http://lama.dev",
+      link: "https://www.pexels.com/",
     },
   ];
   
