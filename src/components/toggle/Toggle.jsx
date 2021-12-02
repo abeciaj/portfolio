d76807084baaf9a4a3c9ef0cfbbcf214c5d28c02
abeciaj <abeciaj23@gmail.com>
2021-12-02 16:43:21 +0800

    Added toggle button.

--- src/components/toggle/Toggle.jsx
+++ src/components/toggle/Toggle.jsx
@@ -1,7 +1,13 @@
+import "./toggle.css"
+import Sun from "../../img/sun.png"
+import Moon from "../../img/moon.png"
+
 const Toggle = () => {
     return (
         <div className="t">
-            
+            <img src={Sun} alt="" className="t-icon" />
+            <img src={Moon} alt="" className="t-icon" />
+            <div className="t-button"></div>
         </div>
     )
 }
